2592173b9bbec8e39ca56f5adb343474ee3ecd2c
kaiorod <caiorodrigo2775@gmail.com>
2021-11-12 22:20:15 -0300

    Finaliza SequenciaPares

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, p = 0;
+
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Numeros pares de 0 até : ");
+            n = int.Parse(Console.ReadLine());
+
+            while(p < n)
+            {
+                Thread.Sleep(50);
+                Console.Write($" {p}");
+                p = p + 2;
+            }
+        }
+    }
+}
